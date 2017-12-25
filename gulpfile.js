fcaaa17198dae0eedac2d75641c87e76ee5566d0
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-24 21:04:10 -0500

    Setup : install and setup blendid

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')
 
 
-gulp.task('sass', function(){
-	return gulp.src('./src/css/**/*.scss', {"base" : "./src/css"})
+gulp.task('sass', () =>
+	gulp.src('./src/css/**/*.scss', {"base" : "./src/css"})
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulp.dest('./build/css'))
-})
+)
 
-gulp.task("watch:sass", function(){
-	return gulp.watch('./src/css/**/*.scss')
-})
+gulp.task("watch:sass", () =>
+	gulp.watch('./src/css/**/*.scss')
+)
