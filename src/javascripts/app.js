6b8c9322f2e54d5ddc6e724f7b45d51e23a46bc0
Shawn Sandy <shawnsandy04@gmail.com>
2018-01-17 21:15:03 -0500

    adds a timeout [feature] confirm-actions function

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,14 +2,14 @@ console.log('app.js has loaded!')
 
 // let test = new TestClass();
 import { u } from "umbrellajs";
+import { setTimeout } from "timers";
 const validate = require("validate");
-validate.init();
-
 const smoothScroll = require("smooth-scroll");
+
+validate.init();
 const scroll = new smoothScroll('a[href*="#"]');
 
 /** scroll to top */
-
 const screenHeight = window.innerHeight;
 const toTop = u(".back-to-top");
 
@@ -21,7 +21,6 @@ window.addEventListener('scroll', (e) => {
  }
 });
 
-
 /**
  * Confirm actions before submit
  * Prompt user before deleting data
@@ -30,19 +29,29 @@ u(".confirm-actions").each((elm, i) => {
 
   if(elm)
   {
-    let val = u(elm).html();
+
+    // grab the default element html value
+    const val = u(elm).html();
     //console.log(val);
 
+    // listen for the click event
     u(elm).on("click", (e) => {
-      console.log("clicked")
+      // set the confirmation value
+      // search for data-confirm-html attribute
+      // or defaults to confirm
       const confirmation = u(elm).data('confirm-html') || "confirm";
 
+      // check if element html value is the same as the confirmation
+      // if is reset to original value else set to confirmation
+      // sets a 5 second timeout for user to respond
+      // or reset the element to original value
       if (confirmation === u(elm).html()) {
         u(elm).html(val);
-        console.log(val);
       } else {
         u(elm).html(confirmation);
-        console.log(val);
+        setTimeout(() => {
+          u(elm).html(val);
+        }, 5000);
         e.preventDefault();
       }
 
