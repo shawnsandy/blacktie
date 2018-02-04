77fd15b72b80b078a480f584b2a096cde41b290e
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-03 20:57:50 -0500

    refactoring moves the test function component

--- src/js/hyper/index.js
+++ src/js/hyper/index.js
@@ -1,40 +1,11 @@
-import { h, app } from "hyperapp";
-import { Button } from "./elements/base";
+import { app } from "hyperapp"
+import { Button } from "./elements/core"
+import { u } from "umbrellajs"
 
-import { u } from "umbrellajs";
+const btn = require("./button-component")
+const top = require("./pageTop")
+const main = app(btn.state, btn.actions, btn.view, document.querySelector(".hyper"));
 
+const main1 = app(btn.state, btn.actions, btn.view, document.querySelector(".hyper-1"));
 
-const test = u("data-element").each(elm => {
-  return 'tested';
-});
-
-const state = {
-  count: 0,
-  name: test
-};
-
-const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 }),
-  data: () => state => ({ name: u(".hyper").find("id") })
-};
-
-const view = (state, actions) => (
-  <div
-    class=""
-    oncreate={e => {
-
-      u("[data-element]").each(elm => {
-        console.log(elm);
-      });
-    }}
-  >
-    <h1 class="title is-1 is-uppercase">
-      {u(".hyper").data("element")} {state.count}
-    </h1>
-    <Button click={actions.down} selectors="elm-blue"> Min </Button> <Button click={actions.up} selectors="elm-blue"> Add </Button>
-  </div>
-);
-
-const main = app(state, actions, view, document.querySelector(".hyper"));
-const main1 = app(state, actions, view, document.querySelector(".hyper-1"));
+const toTop = app(top.state, top.actions, top.view, document.querySelector('.top'))
