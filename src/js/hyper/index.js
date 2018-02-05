7927835fcf8e80a77960a3b51ae385002b9e8597
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-05 01:52:17 -0500

    moves inline commands to getIcon / getClasses functions

--- src/js/hyper/index.js
+++ src/js/hyper/index.js
@@ -4,10 +4,10 @@ import { u } from "umbrellajs"
 
 const btn = require("./button-component")
 const top = require("./page-top")
-const icon = require('./svg-icon')
+const svg = require('./svg-icon')
 
 const main = app(btn.state, btn.actions, btn.view, document.querySelector(".hyper"));
 
 const toTop = app(top.state, top.actions, top.view, document.querySelector('.top'))
 
-const svg = app(icon.state, icon.actions, icon.bytesize, document.querySelector('.icon'))
+const icon = app(svg.state, svg.actions, svg.view, document.querySelector('.icon'))
--- src/js/hyper/svg-icon.js
+++ src/js/hyper/svg-icon.js
@@ -3,43 +3,51 @@ import { u } from "umbrellajs";
 import { Svg } from "./elements/core";
 
 export const state = {
-  icon: "/icons/bytesize-symbols.min.svg#i-chevron-right",
+  icon: "i-chevron-right",
   tag: null,
   classes: "svg-elm",
-}
+  svg_url: "/icons/bytesize-symbols.min.svg#"
+};
 
 export const actions = {
   setTag: tag => state => ({
     tag: null
   }),
+
   setIcon: icon => state => ({
     icon: icon
   }),
+
   setClass: classes => state => ({
     classes: classes
+  }),
+
+  setUrl: svg_url => state => ({
+    svg_url: svg_url
   })
+};
+
+const getIcon = (elm) => {
+  return  u(elm)
+          .parent()
+          .data("icon") || "/icons/bytesize-symbols.min.svg#i-chevron-right";
+}
+
+const getClasses = elm => {
+  return u(elm)
+      .parent()
+      .data("class") || "svg-elm";
 }
 
 export const view = (state, actions) => (
-  <span oncreate={e => {
-   const icon = u(e).parent().data('icon') || "i-chevron-right"
-   const classes = u(e).parent().data('class') || "svg-elm"
-   console.log( icon )
-   actions.setIcon(icon);
-   actions.setClass(classes)
-  }}
-     >
+  <span
+    oncreate={e => {
+      const icon = getIcon(e)
+      const classes = getClasses(e)
+      actions.setIcon(icon);
+      actions.setClass(classes);
+    }}
+  >
     <Svg icon={state.icon} classes={state.classes} />
   </span>
-)
-
-export const bytesize = (state, actions) => (
-  <span oncreate={e => {
-   const icon = u(e).parent().data('icon') || "i-chevron-right"
-   const classes = u(e).parent().data('class') || "svg-elm"
-   actions.setIcon(icon);
-   actions.setClass(classes)
-  }}>
-    <Svg icon={`/icons/bytesize-symbols.min.svg#${state.icon}`} classes={state.classes} />
-  </span>
-)
+);
