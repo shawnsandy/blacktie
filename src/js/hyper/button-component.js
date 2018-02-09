97d5a713a192a3da5ab3b5e52e61a1edc0ee01f2
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-09 18:07:29 -0500

    refactoring button element

--- src/js/hyper/button-component.js
+++ src/js/hyper/button-component.js
@@ -4,22 +4,23 @@ import { u } from "umbrellajs";
 
 export const state = {
   count: 0,
-};
+}
 
 export const actions = {
   down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 })
-};
+  up: () => state => ({ count: state.count + 1 }),
+  label: (val) => state => ({ label: val })
+}
 
 export const view = (state, actions) => (
   <div
     class=""
-    oncreate={ e => {
-
+    oncreate={ (e) => {
+      const title = u(e).find('title').html()
+      const label = u(e).parent().data('element')
     }}
   >
-    <h1 class="title is-1 is-uppercase">{u(".hyper").data("element")}  {state.count}</h1>
+    <h1 class="title is-1 is-uppercase">{state.count}</h1>
     <Button click={actions.down}> Min </Button>   <Button click={actions.up} classes="elm-blue"> Add </Button>
-
   </div>
-);
+)
