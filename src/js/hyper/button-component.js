775686116666070f80a0f7ccd311983647040e9c
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-09 19:23:41 -0500

    Update the layout moves all the components to one section

--- src/js/hyper/button-component.js
+++ src/js/hyper/button-component.js
@@ -4,23 +4,29 @@ import { u } from "umbrellajs";
 
 export const state = {
   count: 0,
-}
+  label: "..."
+};
 
 export const actions = {
   down: () => state => ({ count: state.count - 1 }),
   up: () => state => ({ count: state.count + 1 }),
   label: (val) => state => ({ label: val })
-}
+};
 
 export const view = (state, actions) => (
   <div
     class=""
-    oncreate={ (e) => {
-      const title = u(e).find('title').html()
-      const label = u(e).parent().data('element')
+    oncreate={ e => {
+
     }}
   >
-    <h1 class="title is-1 is-uppercase">{state.count}</h1>
-    <Button click={actions.down}> Min </Button>   <Button click={actions.up} classes="elm-blue"> Add </Button>
+    <h1 class="title is-1 is-uppercase">
+      {state.count}
+    </h1>
+    <Button click={actions.down}> Min </Button>{" "}
+    <Button click={actions.up} classes="elm-blue">
+      {" "}
+      Add{" "}
+    </Button>
   </div>
-)
+);
