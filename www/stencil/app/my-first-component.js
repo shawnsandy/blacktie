339210be30a90a4b61fc404974908b81b0e86ef3
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-14 22:07:49 -0500

    Runs a stencil build concurrently with wibpack dev server

--- www/stencil/app/my-first-component.js
+++ www/stencil/app/my-first-component.js
@@ -1,15 +1,2 @@
 /*! Built with http://stenciljs.com */
-const { h, Context } = window.App;
-
-class MyComponent {
-    render() {
-        return (h("p", null,
-            "My name is ",
-            this.name,
-            "!!!!"));
-    }
-    static get is() { return "my-first-component"; }
-    static get properties() { return { "name": { "type": String, "attr": "name" } }; }
-}
-
-export { MyComponent as MyFirstComponent };
+const{h:t,Context:e}=window.App;class n{render(){return t("p",null,"My name is ",this.name,"!")}static get is(){return"my-first-component"}static get properties(){return{name:{type:String,attr:"name"}}}}export{n as MyFirstComponent};
