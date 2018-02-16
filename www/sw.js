441eafd849136c184b7332b06a8254c9b2310c16
Shawn Sandy <shawnsandy04@gmail.com>
2018-02-15 21:06:56 -0500

    Run build test to determine the bundle seperation, hashfile name removal

--- www/sw.js
+++ www/sw.js
@@ -27,42 +27,42 @@ self.__precacheManifest = [
   },
   {
     "url": "stencil/app.js",
-    "revision": "57fd3313d35fcbbf11a7f6878e579b54"
+    "revision": "18fbc5933b83cc436c92732978230964"
   },
   {
-    "url": "stencil/app/0gvjaxcy.es5.js",
-    "revision": "d5f147bf67110dcaf70a30062f0b0546"
-  },
-  {
-    "url": "stencil/app/0gvjaxcy.js",
-    "revision": "d568d3633bc659b2d0fd85a5bf6dfadf"
-  },
-  {
-    "url": "stencil/app/app.5xfahmc9.js",
-    "revision": "669097b16541d15f586b7ea1009603e8"
-  },
-  {
-    "url": "stencil/app/app.hnteciro.js",
+    "url": "stencil/app/app.core.js",
     "revision": "0dfbf5e2806b8dcb6ee4979d26603ad9"
   },
   {
+    "url": "stencil/app/app.core.pf.js",
+    "revision": "669097b16541d15f586b7ea1009603e8"
+  },
+  {
     "url": "stencil/app/app.registry.json",
-    "revision": "f6fd503cd2752457265ca6e36c9b59ac"
+    "revision": "e9f4ec1a788fcc9655f66d0400f41d61"
   },
   {
-    "url": "stencil/app/gp5xne7w.es5.js",
-    "revision": "11473e869478c16e6dbed02cea347ef5"
+    "url": "stencil/app/card-elm.es5.js",
+    "revision": "2d228472383cba85ce5a1183ecd3bf2c"
   },
   {
-    "url": "stencil/app/gp5xne7w.js",
+    "url": "stencil/app/card-elm.js",
     "revision": "615f0919a1b41ffecccc60fd757fb6bc"
   },
   {
-    "url": "stencil/app/hlvasici.es5.js",
-    "revision": "d3e3542331f7d38ed3d457ead30f11ec"
+    "url": "stencil/app/my-component.es5.js",
+    "revision": "059e36e37773c97de3147161470fd14c"
   },
   {
-    "url": "stencil/app/hlvasici.js",
+    "url": "stencil/app/my-component.js",
+    "revision": "d568d3633bc659b2d0fd85a5bf6dfadf"
+  },
+  {
+    "url": "stencil/app/my-first-component.es5.js",
+    "revision": "f9fd12c8931551284e05965bb4530aa0"
+  },
+  {
+    "url": "stencil/app/my-first-component.js",
     "revision": "f37a87026a84a77722e3e1d79fbbb788"
   }
 ].concat(self.__precacheManifest || []);
