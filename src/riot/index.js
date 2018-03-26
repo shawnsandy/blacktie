d73f62552cab861f1b73df112ea6c2579de2f126
Shawn Sandy <shawnsandy04@gmail.com>
2018-03-25 22:41:27 -0400

    renames img folder to images

--- src/riot/index.js
+++ src/riot/index.js
@@ -8,6 +8,6 @@ import './uploads/bt-upload.tag'
 import './uploads/bt-upload-rounded.tag'
 import './elm/bt-elm-confirmation.tag'
 import './elm/bt-elm-pagetop.tag'
-import './img/bt-image.tag'
+import './images/bt-image.tag'
 
 riot.mount('*')
